35d77e36b38a5a8b0678b7f89168d4492f4d9d3e
Ruczak <igor.sarnowski@gmail.com>
2022-02-23 22:24:41 +0100

    Detached event bus from main script

--- Services/CommunicationService.py
+++ Services/CommunicationService.py
@@ -1,3 +1,4 @@
+import asyncio
 from Misc.sx126x import sx126x
 from ._Service import Service
 import struct
@@ -5,24 +6,28 @@ import struct
 
 class CommunicationService(Service, sx126x):
     def __init__(self, name: str, address: int):
-        sx126x.__init__(self, serial_num = "/dev/ttyS0", freq=868, addr=address, power=22, rssi=False, air_speed=2400, relay=False)
+        sx126x.__init__(self, serial_num="/dev/ttyS0", freq=868, addr=address, power=22, rssi=False, air_speed=2400, relay=False)
         Service.__init__(self, name)
 
+    # encode data to bytes using certain format
     @staticmethod
     def encode(row: tuple, byte_format: str):
         return struct.pack(byte_format, *row)
 
+    # decode bytes to values using certain format
     @staticmethod
     def decode(data: bytes, byte_format: str):
         return struct.unpack(byte_format, data)
 
-    def send(self, address: int, freq: int, row: tuple, byte_format: str) -> None:
-        offset_frequency = int(freq) - (850 if int(freq) > 850 else 410)
+    # send row in specified format by frequency to address
+    async def send(self, address: int, freq: int, row: tuple, byte_format: str) -> None:
+        try:
+            offset_frequency = int(freq) - (850 if int(freq) > 850 else 410)
+            buffer = CommunicationService.encode(row, byte_format)
+            data = bytes([int(address) >> 8]) + bytes([int(address) & 0xff]) + bytes([offset_frequency]) + bytes([self.addr >> 8]) + bytes([self.addr & 0xff]) + bytes([self.offset_freq]) + buffer
 
-        buffer = CommunicationService.encode(row, byte_format)
-
-        data = bytes([int(address) >> 8]) + bytes([int(address) & 0xff]) + bytes([offset_frequency]) + bytes([self.addr >> 8]) + bytes([self.addr & 0xff]) + bytes([self.offset_freq]) + buffer
-
-        sx126x.send(self, data)
-
-        print(f"Sent data to address of ({address}, {freq} MHz), {buffer}")
+            sx126x.send(self, data)
+            print(f"Sent data to {address} / {freq} MHz: {buffer.hex()}")
+        except asyncio.CancelledError:
+            print("Cancelled sending data.")
+            raise
--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def main():
 
     async def save_file_handler(e: UpdateCsvEvent):
         try:
-            file_service.add_to_csv(e.path, e.data)
+            await file_service.add_to_csv(e.path, e.data)
         except asyncio.CancelledError:
             print("Cancelled save file handler.")
             raise
@@ -67,18 +67,18 @@ async def main():
         print("Sea Level Pressure set to:", rec_service.ref_pressure)
 
         while True:
-            await asyncio.sleep(1)
-            # pressure = random.randint(800, 1010)
+            timer = await asyncio.sleep(1)
+
             data = {'timestamp': time.time(), 'temp': sens_service.temp, 'pressure': sens_service.pressure,
                     'lat': gps_service.latitude, 'lon': gps_service.longitude}
-            # data = {'timestamp': time.time(), 'temp': random.randint(-10,10), 'pressure': pressure, 'lat': 0,
-            # 'lon': 0}
-            update_event = UpdateCsvEvent('atm_data.csv', data)
-            send_event = SendCsvEvent(22, 868, tuple(data.values()), "5d")
-            altitude_event = UpdateHeightEvent(data['pressure'])
-            event_bus.emit('saveTempAndPressure', update_event)
-            event_bus.emit('sendTempAndPressure', send_event)
-            event_bus.emit('updateAltitude', altitude_event)
+            # data = {'timestamp': time.time(), 'temp': random.randint(-10,10), 'pressure': random.randint(800,
+            # 1020), 'lat': 0, 'lon': 0}
+
+            asyncio.get_running_loop().create_task(file_service.add_to_csv('atm_data.csv', data))
+            asyncio.get_running_loop().create_task(comm_service.send(22, 868, tuple(data.values()), "5d"))
+            asyncio.get_running_loop().create_task(rec_service.update_altitude(data['pressure']))
+
+            await timer
     except asyncio.CancelledError:
         sdr_service.stop()
         sens_service.stop()
