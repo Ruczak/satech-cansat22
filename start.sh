29df27181418ea9167a8498bc8bf95838db37874
Ruczak <igor.sarnowski@gmail.com>
2022-03-23 21:31:06 +0100

    Changed path to start script

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ sudo stty -F /dev/ttyUSB0 9600
 echo "Set baud rate of /dev/ttyUSB0 to 9600"
 echo "================"
 echo "STARTING PROGRAM"
-sudo python3 ./main.py
+sudo python3 home/pi/satech-cansat22/main.py
